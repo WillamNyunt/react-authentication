cf8aa406e322d581591f9258f037bf24f0356eb3
WillamNyunt <williamnyunt@outlook.com>
2024-04-23 18:55:17 +0100

    Signup process complete

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,5 +1,5 @@
 import AuthForm from '../components/AuthForm';
-import { json, redirect } from 'react-router-dom';
+import { json, redirect} from 'react-router-dom';
 
 function AuthenticationPage() {
   return <AuthForm />;
@@ -9,18 +9,23 @@ export default AuthenticationPage;
 
 export async function action({request}) {
   const mode = new URL(request.url).searchParams.get('mode');
+
+
+  console.log(mode)
   const data = await request.formData();
   const authData = {
     email: data.get('email'),
     password: data.get('password'),
   };
 
-  if (mode !== 'login' || mode !== 'signup') {
+  if (mode !== 'login' && mode !== 'signup') {
     throw json({message: 'Unsupported mode.'}, { status: 422});
   }
 
   if (mode) {
-    const response = await fetch('http://localhost:8080/login' + mode, {
+    const url = `http://localhost:8080/${mode}`;
+    console.log(url)
+    const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify(authData),
       headers: {
@@ -29,7 +34,7 @@ export async function action({request}) {
     });
 
     if (response.status === 422 || response.status === 401) {
-      throw json({ message: 'Authentication failed.'}, { status: 401});
+      return response
     }
 
     if (!response.ok) {
