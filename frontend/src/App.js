1047ea0f9832da0abde68c3bab5bb6556c8e23bc
WillamNyunt <williamnyunt@outlook.com>
2024-04-23 17:31:37 +0100

    Creating flexible route for login/sign-up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,8 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+import Authentication, { action as authAction } from './pages/Authentication';
+
 
 const router = createBrowserRouter([
   {
@@ -21,6 +23,11 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
+      {
+        path: 'auth',
+        element: <Authentication />,
+        action: authAction,
+      },
       {
         path: 'events',
         element: <EventsRootLayout />,
@@ -51,7 +58,7 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
-          },
+          }
         ],
       },
       {
--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,41 @@
 import AuthForm from '../components/AuthForm';
+import { json, redirect } from 'react-router-dom';
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({request}) {
+  const mode = new URL(request.url).searchParams.get('mode');
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
+
+  if (mode !== 'login' || mode !== 'signup') {
+    throw json({message: 'Unsupported mode.'}, { status: 422});
+  }
+
+  if (mode) {
+    const response = await fetch('http://localhost:8080/login' + mode, {
+      method: 'POST',
+      body: JSON.stringify(authData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.status === 422 || response.status === 401) {
+      throw json({ message: 'Authentication failed.'}, { status: 401});
+    }
+
+    if (!response.ok) {
+      throw json({ message: 'Error. Could not authenticate user.'}, { status: 500});
+    }
+
+    return redirect('/')
+  }
+}
